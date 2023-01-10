cdd0cfcafec9e97dd9af2c8889a246df3d2741e5
Bartosz Nowak <bartekn337@gmail.com>
2023-01-10 14:29:11 +0100

    convert to sse

--- server/worker/src/api/public/ohlc.rs
+++ server/worker/src/api/public/ohlc.rs
@@ -1,3 +1,25 @@
-pub async fn root() -> &'static str {
-    "ohlc endpoint"
+use axum::{
+    headers::UserAgent,
+    extract::TypedHeader,
+    response::sse::{Event, Sse},
+};
+use futures::stream::{self, Stream};
+use std::{convert::Infallible, time::Duration};
+use tokio_stream::StreamExt as _;
+
+pub async fn root(
+    TypedHeader(user_agent): TypedHeader<UserAgent>
+) -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
+    println!("`{}` connected", user_agent.as_str());
+
+    // A `Stream` that repeats an event every second
+    let stream = stream::repeat_with(|| Event::default().data("hi it is ohlc endpoint"))
+        .map(Ok)
+        .throttle(Duration::from_secs(1));
+
+    Sse::new(stream).keep_alive(
+        axum::response::sse::KeepAlive::new()
+            .interval(Duration::from_secs(1))
+            .text("keep-alive-text"),
+    )
 }
