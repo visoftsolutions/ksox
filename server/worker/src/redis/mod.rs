4b7945ea7b788e869be346ce21d56223c97b86ef
Bartosz Nowak <bartekn337@gmail.com>
2023-01-15 20:13:37 +0100

    docker workinprogress

--- server/worker/src/redis/mod.rs
+++ server/worker/src/redis/mod.rs
@@ -1,7 +1,5 @@
 use anyhow::{Ok, Result};
 
 pub fn get_redis_client() -> Result<redis::Client> {
-    let uri = std::env::var("REDIS_URL").unwrap_or_else(|_| "redis://ksox-redis/".to_string());
-    let client = redis::Client::open(uri)?;
-    Ok(client)
+    Ok(redis::Client::open(std::env!("REDIS_URL"))?)
 }
