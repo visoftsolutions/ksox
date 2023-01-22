fd383629dd703d979d813f54740ee3697d63666d
Paweł <pavlnowak@gmail.com>
2023-01-22 11:24:23 +0100

    min-width-900 for small screens

--- web/src/routes/(landing).tsx
+++ web/src/routes/(landing).tsx
@@ -4,8 +4,8 @@ import Footer from "~/components/landing/Footer";
 
 export default function Landing() {
   return (
-    <div class="bg-background-landing w-screen">
-      <div class="max-w-[1200px] min-h-screen m-auto flex flex-col">
+    <div class="bg-background-landing min-w-fit">
+      <div class="max-w-[1200px] min-w-[900px] min-h-screen m-auto flex flex-col">
         <Header />
         <Main />
         <Footer class="mb-[170px]" />
