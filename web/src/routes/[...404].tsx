5dc2aa91bf4677e7ce2cd45928baa6ba17658667
Paweł <pavlnowak@gmail.com>
2023-01-30 17:55:25 +0100

    404 page header fix

--- web/src/routes/[...404].tsx
+++ web/src/routes/[...404].tsx
@@ -3,7 +3,7 @@ import Header from "~/components/landing/Header";
 export default function NotFound() {
   return (
     <div class="bg-background">
-      <div class="flex min-h-screen flex-col p-6">
+      <div class="m-auto flex min-h-screen max-w-7xl flex-col p-6">
         <Header />
         <div class="mt-32 flex justify-center text-center text-5xl text-white">
           We are working on that...
