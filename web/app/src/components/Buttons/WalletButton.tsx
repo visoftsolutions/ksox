231311dbc501d681979566dcf888a1aa23ff6ec6
Paweł <neotheprogramist@proton.me>
2023-03-27 16:08:00 +0200

    databases as non helm charts

--- web/app/src/components/Buttons/WalletButton.tsx
+++ web/app/src/components/Buttons/WalletButton.tsx
@@ -1,15 +1,44 @@
 import { joinPaths } from "solid-start/islands/server-router";
 import { base } from "~/root";
+import { UserSession } from "~/routes/(app)";
+import { useContext } from "solid-js";
 
-export interface WalletButtonComponent {
-  onClick?: (e: MouseEvent) => void;
-}
+const projectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID;
 
-export default function WalletButton(props: WalletButtonComponent) {
+export default function WalletButton() {
+  const [userSession, setUserSession] = useContext(UserSession);
+
+  // const walletConnect = async () => {
+  //   const web3Modal = new Web3Modal({
+  //     walletConnectVersion: 2,
+  //     projectId,
+  //     standaloneChains: ["eip155:1"],
+  //   });
+
+  //   let signClient = await SignClient.init({ projectId });
+
+  //   const { uri, approval } = await signClient.connect({
+  //     requiredNamespaces: {
+  //       eip155: {
+  //         methods: ["eth_sign"],
+  //         chains: ["eip155:1"],
+  //         events: ["accountsChanged"],
+  //       },
+  //     },
+  //   });
+
+  //   if (uri) {
+  //     web3Modal.openModal({ uri, standaloneChains: ["eip155:1"] });
+  //     let sessionNamespace = await approval();
+  //     console.log(sessionNamespace);
+  //     web3Modal.closeModal();
+  //   }
+  // };
+
   return (
     <div
       class="grid cursor-pointer select-none grid-cols-[auto_auto] grid-rows-[1fr] items-center justify-center gap-4 px-4"
-      onClick={(e) => props.onClick?.(e)}
+      onClick={async () => {}}
     >
       <div class="text-mainmenu-wallet font-normal">CONNECT WALLET</div>
       <img src={joinPaths(base, "gfx/metamask.webp")} class="m-auto w-[22px]" />
--- web/app/src/routes/(app).tsx
+++ web/app/src/routes/(app).tsx
@@ -6,34 +6,42 @@ import Orderbook from "~/components/OrderBook";
 import SideMenu from "~/components/SideMenu";
 import Submit from "~/components/Submit";
 import Trades from "~/components/Trades";
+import { createSignal } from "solid-js";
+import { createContext, Accessor, Setter } from "solid-js";
+
+export const UserSession = createContext([() => "", () => {}] as [Accessor<string>, Setter<string>]);
 
 export default function Home() {
+  const [userSession, setUserSession] = createSignal("");
+
   return (
-    <main class="grid h-screen w-screen grid-cols-[72px_240px_1fr_260px_260px] grid-rows-[48px_1fr_280px] gap-[1px] overflow-auto bg-gray-1 font-sanspro text-white">
-      <div class="col-start-2 col-end-6 row-start-1 row-end-2 bg-gray-2">
-        <MainMenu />
-      </div>
-      <div class="col-start-1 col-end-2 row-start-1 row-end-4 bg-gray-1">
-        <SideMenu />
-      </div>
-      <div class="col-start-2 col-end-3 row-start-2 row-end-3 bg-gray-2 xl:hidden">
-        <Markets />
-      </div>
-      <div class="col-start-3 col-end-4 row-start-2 row-end-3 bg-gray-1 lg:col-end-5 xl:col-start-2">
-        <Chart />
-      </div>
-      <div class="col-start-4 col-end-5 row-start-2 row-end-3 bg-gray-2 lg:col-start-5">
-        <Orderbook />
-      </div>
-      <div class="col-start-5 col-end-6 row-start-2 row-end-3 bg-gray-2 lg:hidden">
-        <Trades />
-      </div>
-      <div class="col-start-2 col-end-4 row-start-3 row-end-4 bg-gray-2 lg:col-end-5">
-        <State />
-      </div>
-      <div class="col-start-4 col-end-6 row-start-3 row-end-4 bg-gray-2 lg:col-start-5">
-        <Submit buy={{ available: "22,234.12 USDT" }} sell={{ available: "22,234.12 USDT" }} />
-      </div>
-    </main>
+    <UserSession.Provider value={[userSession, setUserSession]}>
+      <main class="grid h-screen w-screen grid-cols-[72px_240px_1fr_260px_260px] grid-rows-[48px_1fr_280px] gap-[1px] overflow-auto bg-gray-1 font-sanspro text-white">
+        <div class="col-start-2 col-end-6 row-start-1 row-end-2 bg-gray-2">
+          <MainMenu />
+        </div>
+        <div class="col-start-1 col-end-2 row-start-1 row-end-4 bg-gray-1">
+          <SideMenu />
+        </div>
+        <div class="col-start-2 col-end-3 row-start-2 row-end-3 bg-gray-2 xl:hidden">
+          <Markets />
+        </div>
+        <div class="col-start-3 col-end-4 row-start-2 row-end-3 bg-gray-1 lg:col-end-5 xl:col-start-2">
+          <Chart />
+        </div>
+        <div class="col-start-4 col-end-5 row-start-2 row-end-3 bg-gray-2 lg:col-start-5">
+          <Orderbook />
+        </div>
+        <div class="col-start-5 col-end-6 row-start-2 row-end-3 bg-gray-2 lg:hidden">
+          <Trades />
+        </div>
+        <div class="col-start-2 col-end-4 row-start-3 row-end-4 bg-gray-2 lg:col-end-5">
+          <State />
+        </div>
+        <div class="col-start-4 col-end-6 row-start-3 row-end-4 bg-gray-2 lg:col-start-5">
+          <Submit buy={{ available: "22,234.12 USDT" }} sell={{ available: "22,234.12 USDT" }} />
+        </div>
+      </main>
+    </UserSession.Provider>
   );
 }
