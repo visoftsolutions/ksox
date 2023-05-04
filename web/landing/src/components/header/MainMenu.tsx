cde7828bd8a0acd797f102d874cf3f238c102f4c
Bartosz Nowak <bartoszn337@proton.me>
2023-05-04 18:01:57 +0200

    footer fixup

--- web/landing/src/components/header/MainMenu.tsx
+++ web/landing/src/components/header/MainMenu.tsx
@@ -9,7 +9,8 @@ export default function MainMenu() {
       </a>
       <a
         class="main-menu-button font-medium text-links_new hover:text-text-1"
-        href="/#section-1"
+        href="/whitepaper/ksox-whitepaper.pdf"
+        target="_blank"
       >
         Whitepaper
       </a>
@@ -21,23 +22,12 @@ export default function MainMenu() {
       </a>
       <a
         class="main-menu-button font-medium text-links_new hover:text-text-1"
-<<<<<<< Updated upstream
-        href="/whitepaper/ksox-whitepaper.pdf"
-        target="_blank"
-      >
-        Whitepaper
-      </a>
-      <a
-        class="main-menu-button font-medium text-links_new hover:text-text-1"
         href="#section-4"
-=======
-        href="/#section-4"
->>>>>>> Stashed changes
       >
         Contact
       </a>
       <a
-        class="main-menu-button rounded-full bg-buttonbg_new px-5 py-2 font-medium text-text-1 hover:bg-text-1 hover:text-buttonbg_new "
+        class="main-menu-button rounded-full bg-buttonbg_new px-5 py-2 font-medium text-text-1 hover:bg-text-1 hover:text-buttonbg_new"
         href="/app"
       >
         Launch App
