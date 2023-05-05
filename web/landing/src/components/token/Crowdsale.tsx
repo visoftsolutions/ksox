3da9a67c70156e60f6a6cea667afd0c743b4a462
Bartosz Nowak <bartoszn337@proton.me>
2023-05-05 19:13:18 +0200

    token single bucket design

--- web/landing/src/components/token/Crowdsale.tsx
+++ web/landing/src/components/token/Crowdsale.tsx
@@ -1,73 +1,49 @@
-import Spacing from "../Spacing";
-import Bucket from "./Bucket";
 import ProgressBar from "./ProgressBar";
 import TimerTile from "./TimerTile";
 
 export default function Crowdsale () {
     return (
-        <div class="grid grid-rows-[auto_auto_auto] font-lexend text-text-1 gap-24">
+        <div class="grid grid-rows-[auto_auto] font-lexend text-text-1 gap-24">
             <div class="row-start-1 row-end-2 text-center text-4xl font-medium">KSXT Crowdsale</div>
 
-            <div class="grid grid-cols-[1fr_1fr] grid-rows-[auto_auto] row-start-2 row-end-3 gap-5 gap-y-16">
-                <div class="col-start-1 col-end-2 row-start-1 row-end-2 grid grid-flow-col gap-[1px]">
-                        <Bucket fill={0.1}/>
-                        <Bucket fill={0.2}/>
-                        <Bucket fill={0.4}/>
-                        <Bucket fill={0.1}/>
-                        <Bucket fill={0.2}/>
-                        <Bucket fill={0.1}/>
-                        <Bucket fill={0.2}/>
-                        <Bucket fill={1}/>
-                        <Bucket fill={0.1}/>
-                        <Bucket fill={0.2}/>
-                        <Bucket fill={1}/>
-                        <Bucket fill={0.2}/>
-                        <Bucket fill={0.4}/>
-                        <Bucket fill={0.1}/>
-                        <Bucket fill={0.2}/>
-                        <Bucket fill={0.1}/>
-                        <Bucket fill={0.2}/>
-                        <Bucket fill={0.4}/>
-                        <Bucket fill={0.42}/>
-                        <Bucket fill={0.7}/>
-                        <Bucket fill={0.0}/>
-                        <Bucket fill={0.0}/>
-                        <Bucket fill={0.0}/>
-                        <Bucket fill={0.0}/>
-                        <Bucket fill={0.0}/>
-                        <Bucket fill={0.0}/>
-                        <Bucket fill={0.0}/>
-                        <Bucket fill={0.0}/>
-                        <Bucket fill={0.0}/>
-                        <Bucket fill={0.0}/>
-                </div>
+            <div class="grid grid-cols-[1fr_1fr] row-start-2 row-end-3 gap-5 items-stretch">
                 
-                <div class="col-start-2 col-end-3 row-start-1 row-end-2 text-2xl font-medium grid grid-rows-2 justify-center items-center px-16">
-                    <div class="text-center">SALE ENDS IN</div>
-                    <div class="grid grid-cols-4 justify-around items-center gap-4">
+                <div class="col-start-1 col-end-2 grid grid-rows-[auto_1fr_100px] gap-8 px-16 justify-items-stretch">
+                    <div class="row-start-1 row-end-2 text-2xl font-medium text-center px-8">
+                        SALE ENDS IN
+                    </div>
+
+                    <div class="row-start-2 row-end-3 text-2xl font-medium grid grid-cols-4 justify-around items-center gap-4 text-center">
                         <TimerTile name="days" value="12"/>
                         <TimerTile name="hours" value="01"/>
                         <TimerTile name="minutes" value="10"/>
                         <TimerTile name="seconds" value="03"/>
                     </div>
-                </div>
 
-                <div class="col-start-1 col-end-2 row-start-2 row-end-3 grid items-center justify-center self-center">
-                    <div class="grid grid-flow-col justify-center items-center">
-                        <div class="p-1">Bucket #{21} filled in:</div>
-                        <div class="p-1 ml-5 text-4xl">{(Math.floor(0.42*10000)/100).toFixed(2)}%</div>
+                    <div class="row-start-3 row-end-4 self-center">
+                        <ProgressBar fill={0.1}/>
                     </div>
                 </div>
 
-                <div class="col-start-2 col-end-3 row-start-2 row-end-3 self-center">
-                    <div class="px-16">
+                <div class="col-start-2 col-end-3 grid grid-rows-[auto_1fr_100px] gap-8 px-16 justify-items-stretch">
+                    <div class="row-start-1 row-end-2 text-2xl font-medium text-center px-8">
+                        OPEN BUCKET 7/32
+                    </div>
+
+                    <div class="row-start-2 row-end-3 self-center">
                         <ProgressBar fill={0.8}/>
                     </div>
+
+                    <div class="row-start-3 row-end-4 self-center">
+                        <div class="rounded-full token-linear-wipe-button p-[11px_32px] text-center font-lexend text-hero-button font-medium text-text-1 md:p-[16px_40px]">
+                            Buy KSXT Token
+                        </div>
+                    </div>
                 </div>
+
+
             </div>
-            <div class="row-start-3 row-end-4 rounded-full token-linear-wipe-button p-[11px_32px] text-center font-lexend text-hero-button font-medium text-text-1 md:p-[16px_40px]">
-                Buy KSXT Token
-            </div>
+            
         </div>
     );
 }
--- web/landing/src/components/token/ProgressBar.tsx
+++ web/landing/src/components/token/ProgressBar.tsx
@@ -1,12 +1,47 @@
+import { createEffect, onMount } from "solid-js";
+
 export interface ProgressBarProps {
     fill: number;
 }
 
 export default function ProgressBar (props: ProgressBarProps) {
+    let sliderDOM!: HTMLInputElement;
+    let popupDOM!: HTMLInputElement;
+
+    function popupPosition(sliderDOM: HTMLInputElement, popupDOM: HTMLElement, value: number) {
+        const sliderRect = sliderDOM.getBoundingClientRect();
+        const popupRect = popupDOM.getBoundingClientRect();
+        return Math.max(
+        Math.min(
+            sliderRect.width * value - popupRect.width / 2,
+            sliderRect.width - popupRect.width
+        ),0
+        );
+    }
+
+    createEffect(() => {
+        const popupRect = popupDOM.getBoundingClientRect();
+        popupDOM.style.top = - popupRect.height - 5 + "px";
+        const left = popupPosition(sliderDOM, popupDOM, props.fill);
+        popupDOM.style.left = left + "px";
+    })
+
+    const handler = () => {
+        const popupRect = popupDOM.getBoundingClientRect();
+        popupDOM.style.top = - popupRect.height - 10 + "px";
+        const left = popupPosition(sliderDOM, popupDOM, props.fill);
+        popupDOM.style.left = left + "px";
+    }
+
+    onMount(() => {
+        window.addEventListener('resize', handler);
+      });
+
     return (
-        <div class="relative rounded-sm overflow-clip h-8 stripes">
-            <div class="absolute left-0 top-0 bottom-0 rounded-sm token-linear-wipe-button"
-                style={{right: ((1-Math.min(1, Math.max(0, props.fill)))*100).toString()+"%"}}
+        <div class="relative rounded-sm h-8 stripes" ref={sliderDOM}>
+            <div class="absolute text-white font-bold text-xs duration-100 transition-all" ref={popupDOM}>{((Math.min(1, Math.max(0, props.fill)))*100).toFixed(2)+"%"}</div>
+            <div class="absolute left-0 top-0 bottom-0 rounded-sm token-linear-wipe-button duration-100 transition-all"
+                style={{right: ((1-Math.min(1, Math.max(0, props.fill)))*100).toFixed(2)+"%"}}
             />
         </div>
     );
