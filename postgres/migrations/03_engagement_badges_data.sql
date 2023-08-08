0c40b35486b032ae6ce83192317e0d1291beba3f
wigya <amajewski505@gmail.com>
2023-08-08 15:18:41 +0200

    add badges data to SQL db

---
+++ postgres/migrations/03_engagement_badges_data.sql
@@ -0,0 +1,52 @@
+-- MAKER BADGES
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('First Maker', 'Maker', 'Badge for performing first maker trade', 1);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Maker Apprentice', 'Maker', 'Badge for performing ten maker trades', 10);
+
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Maker Aficionado', 'Maker', 'Badge for performing hundred maker trades', 100);
+
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Maker Avenger', 'Maker', 'Badge for performing five hundred maker trades', 500);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Maker Ace', 'Maker', 'Badge for performing thousand maker trades', 1000);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Maker Avalanche', 'Maker', 'Badge for performing five thousand maker trades', 5000);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Maker Overlord', 'Maker', 'Badge for performing ten thousand maker trades', 10000);
+
+-- TAKER BADGES
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('First Taker', 'Taker', 'Badge for performing first taker trade', 1);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Taker Beginner', 'Taker', 'Badge for performing ten taker trades', 10);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Taker Bandit', 'Taker', 'Badge for performing hundred taker trades', 100);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Taker Boss', 'Taker', 'Badge for performing five hundred taker trades', 500);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Taker Baron', 'Taker', 'Badge for performing thousand taker trades', 1000);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Taker Bandwagon', 'Taker', 'Badge for performing five thousand taker trades', 5000);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Taker Behemoth', 'Taker', 'Badge for performing ten thousand taker trades', 10000);
+
+-- TRADE BADGES
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('First Trade', 'Trade', 'Badge for performing first trade', 1);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Trade Novice', 'Trade', 'Badge for performing ten trades', 10);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Trade Tornado', 'Trade', 'Badge for performing one hundred trades', 100);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Trade Tyrant', 'Trade', 'Badge for performing five hundred trades', 500);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Trade Mogul', 'Trade', 'Badge for performing thousand trades', 1000);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Trade Magnate', 'Trade', 'Badge for performing five thousand trades', 5000);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Trade Overlord', 'Trade', 'Badge for performing ten thousand trades', 10000);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Trade Legend', 'Trade', 'Badge for performing fifty thousand trades', 50000);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Trade Titan', 'Trade', 'Badge for performing hundred thousand trades', 100000);
+
+-- TRANSFER BADGES
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('First Transfer', 'Transfer', 'Badge for performing first transfer', 1);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Transfer Rookie', 'Transfer', 'Badge for performing ten transfers', 10);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Transfer Trooper', 'Transfer', 'Badge for performing one hundred transfers', 100);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Transfer Courier', 'Transfer', 'Badge for performing five hundred transfers', 500);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Transfer Magician', 'Transfer', 'Badge for performing thousand transfers', 1000);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Transfer Wizard', 'Transfer', 'Badge for performing five thousand transfers', 5000);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Transfer Terminator', 'Transfer', 'Badge for performing ten thousand transfers', 10000);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Transfer Legend', 'Transfer', 'Badge for performing fifty thousand transfers', 50000);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Transfer Titan', 'Transfer', 'Badge for performing hundred thousand transfers', 100000);
+
+-- VALUT BADGES
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('First Token', 'Valut', 'Badge for having first token', 1);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Token Tourist', 'Valut', 'Badge for having five tokens', 5);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Token Collector', 'Valut', 'Badge for having ten tokens', 10);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Token Tamer', 'Valut', 'Badge for having twenty five tokens', 25);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Token Hoarder', 'Valut', 'Badge for having fifty tokens', 50);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Token Diversifier', 'Valut', 'Badge for having seventy five tokens', 75);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Token Magnate', 'Valut', 'Badge for having hundred tokens', 100);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Token Overlord', 'Valut', 'Badge for having five hundred tokens', 500);
+INSERT INTO "engagement"."badges" ("name", "family", "description", "value") VALUES ('Token Titan', 'Valut', 'Badge for having thousand tokens', 1000);
