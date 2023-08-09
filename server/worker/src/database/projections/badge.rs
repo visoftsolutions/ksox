80ed007121b996fb5b7329d91a741e468e7056de
wigya <amajewski505@gmail.com>
2023-08-09 11:45:25 +0200

    add worker badge projection

--- server/worker/src/database/projections/badge.rs
+++ server/worker/src/database/projections/badge.rs
@@ -1,1 +1,11 @@
-pub use engagement::database::projections::badge::*;
+use chrono::{DateTime, Utc};
+use sqlx::types::Uuid;
+
+pub struct Badge {
+    pub id: Uuid,
+    pub name: String,
+    pub family: String,
+    pub description: String,
+    pub value: u64,
+    pub created_at: DateTime<Utc>,
+}
