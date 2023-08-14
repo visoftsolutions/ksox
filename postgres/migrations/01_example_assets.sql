63b00e8cfedfbcf7913072e8a4782f7f6aa9afa7
Paweł <neotheprogramist@proton.me>
2023-08-14 12:44:26 +0200

    fix checksum address

--- postgres/migrations/01_example_assets.sql
+++ postgres/migrations/01_example_assets.sql
@@ -1,5 +1,5 @@
 INSERT INTO "assets" ("name", "address", "decimals", "symbol", "maker_fee", "taker_fee") VALUES
-('TokenPermit', '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512', (1000000000000000000, 1), 'TOKP', (0, 1), (0, 1));
+('TokenPermit', '0xe7f1725e7734ce288f8367e1bb143e90bb3f0512', (1000000000000000000, 1), 'TOKP', (0, 1), (0, 1));
 
 INSERT INTO "assets" ("name", "address", "decimals", "symbol", "maker_fee", "taker_fee") VALUES
-('Wrapped Ether', '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0', (1000000000000000000, 1), 'WETH', (0, 1), (0, 1));
+('Wrapped Ether', '0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0', (1000000000000000000, 1), 'WETH', (0, 1), (0, 1));
