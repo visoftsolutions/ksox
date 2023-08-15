0668cae8307dadbb2481d4a451d6ea4be11ca436
Paweł <neotheprogramist@proton.me>
2023-08-15 12:33:21 +0200

    add blockchain tokens

--- blockchain/apps/treasury/scripts/deploy.ts
+++ blockchain/apps/treasury/scripts/deploy.ts
@@ -3,23 +3,30 @@ import { ethers } from "hardhat";
 async function main() {
   const [owner] = await ethers.getSigners();
 
+  const TokenFactory = await ethers.getContractFactory("Token");
+
+  const bitcoinToken = await TokenFactory.deploy("Wrapped Bitcoin", "BTC");
+  await bitcoinToken.waitForDeployment();
+  await (await bitcoinToken.mint(owner, 1_000_000n * 10n ** 18n)).wait();
+  console.log(
+    "bitcoinToken Contract Address: ",
+    await bitcoinToken.getAddress()
+  );
+
+  const etherToken = await TokenFactory.deploy("Wrapped Ether", "ETH");
+  await etherToken.waitForDeployment();
+  await (await etherToken.mint(owner, 1_000_000n * 10n ** 18n)).wait();
+  console.log("etherToken Contract Address: ", await etherToken.getAddress());
+
+  const usdtToken = await TokenFactory.deploy("Tether USD", "USDT");
+  await usdtToken.waitForDeployment();
+  await (await usdtToken.mint(owner, 1_000_000n * 10n ** 18n)).wait();
+  console.log("usdtToken Contract Address: ", await usdtToken.getAddress());
+
   const TreasuryFactory = await ethers.getContractFactory("Treasury");
   const treasury = await TreasuryFactory.deploy("Treasury", owner);
   await treasury.waitForDeployment();
   console.log("Treasury Contract Address: ", await treasury.getAddress());
-
-  const TokenPermitFactory = await ethers.getContractFactory("TokenPermit");
-  const tokenPermit = await TokenPermitFactory.deploy("TokenPermit", "TOKP");
-  await tokenPermit.waitForDeployment();
-  console.log("tokenPermit Contract Address: ", await tokenPermit.getAddress());
-
-  const WethFactory = await ethers.getContractFactory("WETH");
-  const weth = await WethFactory.deploy("Wrapped Ether", "WETH");
-  await weth.waitForDeployment();
-  console.log("weth Contract Address: ", await weth.getAddress());
-
-  await (await tokenPermit.mint(owner.address, 100000n * 10n ** 18n)).wait();
-  console.log(`tokenPermit: ${owner.address} balance: ${await tokenPermit.balanceOf(owner.address)}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- postgres/migrations/01_example_assets.sql
+++ postgres/migrations/01_example_assets.sql
@@ -1,5 +1,8 @@
 INSERT INTO "assets" ("name", "address", "decimals", "symbol", "maker_fee", "taker_fee") VALUES
-('TokenPermit', '0xe7f1725e7734ce288f8367e1bb143e90bb3f0512', (1000000000000000000, 1), 'TOKP', (0, 1), (0, 1));
+('Wrapped Bitcoin', '0x5fbdb2315678afecb367f032d93f642f64180aa3', (1000000000000000000, 1), 'BTC', (0, 1), (0, 1));
 
 INSERT INTO "assets" ("name", "address", "decimals", "symbol", "maker_fee", "taker_fee") VALUES
-('Wrapped Ether', '0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0', (1000000000000000000, 1), 'WETH', (0, 1), (0, 1));
+('Wrapped Ether', '0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0', (1000000000000000000, 1), 'ETH', (0, 1), (0, 1));
+
+INSERT INTO "assets" ("name", "address", "decimals", "symbol", "maker_fee", "taker_fee") VALUES
+('Tether USD', '0xdc64a140aa3e981100a9beca4e685f962f0cf6c9', (1000000000000000000, 1), 'USDT', (0, 1), (0, 1));
