675d41ec7507abfa6eb94031908ce986e25c972b
Paweł <neotheprogramist@proton.me>
2023-08-17 18:00:22 +0200

    mint to all accounts

--- blockchain/apps/treasury/scripts/deploy.ts
+++ blockchain/apps/treasury/scripts/deploy.ts
@@ -7,7 +7,6 @@ async function main() {
 
   const bitcoinToken = await TokenFactory.deploy("Wrapped Bitcoin", "BTC");
   await bitcoinToken.waitForDeployment();
-  await (await bitcoinToken.mint(owner, 1_000_000n * 10n ** 18n)).wait();
   console.log(
     "bitcoinToken Contract Address: ",
     await bitcoinToken.getAddress()
@@ -15,18 +14,36 @@ async function main() {
 
   const etherToken = await TokenFactory.deploy("Wrapped Ether", "ETH");
   await etherToken.waitForDeployment();
-  await (await etherToken.mint(owner, 1_000_000n * 10n ** 18n)).wait();
   console.log("etherToken Contract Address: ", await etherToken.getAddress());
 
   const usdtToken = await TokenFactory.deploy("Tether USD", "USDT");
   await usdtToken.waitForDeployment();
-  await (await usdtToken.mint(owner, 1_000_000n * 10n ** 18n)).wait();
   console.log("usdtToken Contract Address: ", await usdtToken.getAddress());
 
   const TreasuryFactory = await ethers.getContractFactory("Treasury");
   const treasury = await TreasuryFactory.deploy("Treasury", owner);
   await treasury.waitForDeployment();
   console.log("Treasury Contract Address: ", await treasury.getAddress());
+
+  const tokens = [bitcoinToken, etherToken, usdtToken];
+  const accounts = await ethers.getSigners();
+  const value = 1_000_000_000n * 10n ** 18n;
+  let transactionResponses = [];
+  for (let i = 0; i < tokens.length; i += 1) {
+    const tokenName = await tokens[i].name();
+    for (let j = 0; j < accounts.length; j += 1) {
+      const account = accounts[i];
+      transactionResponses.push(
+        await usdtToken.mint(account.address, 1_000_000_000n * 10n ** 18n)
+      );
+      console.log(`Minting ${value} of ${tokenName} to ${account.address}`);
+    }
+  }
+  console.log("Finishing...");
+  for (let i = 0; i < transactionResponses.length; i += 1) {
+    await transactionResponses[i].wait();
+  }
+  console.log("Finished");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
