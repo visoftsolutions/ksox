6b243c2fc50dee0d1868584eedc7d2fae7284b22
Bartosz Nowak <bartekn337@gmail.com>
2023-08-24 12:17:02 +0200

    frontend improvements

--- web/apps/processor/src/components/Atoms/Picture.tsx
+++ web/apps/processor/src/components/Atoms/Picture.tsx
@@ -1,3 +1,4 @@
+import { Match, Switch } from "solid-js";
 import { joinPaths } from "solid-start/islands/server-router";
 import { base } from "~/root";
 
@@ -6,19 +7,31 @@ export interface IPicture {
   alt?: string;
   size?: number;
   class?: string;
+  skeleton: boolean
 }
 
 export default function Picture(props: IPicture) {
   return (
-    <div class={`rounded-full ${props.class}`}>
-      <img
-        src={joinPaths(base, props.src)}
-        alt={props.alt || ""}
-        style={{
+    <div class={`rounded-full aspect-square ${props.class}`}>
+      <Switch>
+        <Match when={!props.skeleton}>
+          <img
+          src={joinPaths(base, props.src)}
+          alt={props.alt || ""}
+          style={{
+            width: `${props.size || 42}px`,
+            height: `${props.size || 42}px`,
+          }}/>
+        </Match>
+        <Match when={props.skeleton}>
+        <div style={{
           width: `${props.size || 42}px`,
           height: `${props.size || 42}px`,
-        }}
-      />
-    </div>
+
+          }} class="bg-r-blue-light-backdrop dark:bg-r-blue-dark-backdrop aspect-square rounded-full"></div>
+        </Match>
+      </Switch>
+      
+      </div>
   );
 }
--- web/apps/processor/src/components/Home/CurrencyDisplay.tsx
+++ web/apps/processor/src/components/Home/CurrencyDisplay.tsx
@@ -3,7 +3,7 @@ import { joinPaths } from "solid-start/islands/server-router";
 import Picture from "~/components/Atoms/Picture";
 import { base } from "~/root";
 import ArrowDownButton from "~/components/Atoms/Buttons/ArrowDownButton";
-import { Index, Show, createMemo, createSignal } from "solid-js";
+import { Index, Show, createEffect, createMemo, createSignal, onMount } from "solid-js";
 import HalfScreenModal from "~/components/Modals/HalfScreenModal";
 import Currency from "~/components/Atoms/Currency";
 import { useAssets } from "@packages/components/providers/AssetsProvider";
@@ -13,24 +13,28 @@ export default function CurrencyDisplay() {
   const [modal, setModal] = createSignal<boolean>(false);
   const assets = useAssets();
   const assetsList = createMemo(() => [...assets().values()]);
+  
+  createEffect(() => {
+    selectedAsset.setSelectedAsset(assetsList()[0])
+  })
 
   return (
     <div>
       <div class="px-4 pt-4">
-        <div class="flex flex-row justify-between">
-          <div>
-            <div class="flex flex-row items-center">
-              <p class="text-bold font-sans text-3xl text-r-light-text dark:text-r-dark-text">
-                {0.001}
-              </p>
-              <p class="pl-2 font-sans text-3xl text-r-dark-secondary-text">
-                {selectedAsset.selectedAsset()?.symbol}
-              </p>
-              <ArrowDownButton class="m-1" onClick={() => setModal(true)} />
-            </div>
-            <p class="text-sans text-sm text-r-dark-secondary-text">
-              {selectedAsset.selectedAsset()?.name}
+        <div class="grid grid-cols-[1fr_auto] justify-between">
+          <div class="grid grid-cols-3 grid-rows-2 justify-self-start gap-1">
+            <p class="col-start-1 col-end-2 row-start-1 row-end-2  text-bold font-sans text-3xl text-r-light-text dark:text-r-dark-text">
+              {selectedAsset.selectedAsset() != undefined ? 0.001 : "---"}
             </p>
+            <p class="col-start-2 col-end-3 row-start-1 row-end-2 font-sans text-3xl text-r-dark-secondary-text">
+              {selectedAsset.selectedAsset()?.symbol ?? "---"}
+            </p>
+            <p class="col-start-1 col-end-4 row-start-2 row-end-3 text-sans text-sm text-r-dark-secondary-text">
+              {selectedAsset.selectedAsset()?.name ?? "---"}
+            </p>
+            <div class="col-start-3 col-end-4 row-start-1 row-end-2 self-center justify-self-start">
+              <ArrowDownButton class="" onClick={() => setModal(true)} />
+            </div>
           </div>
           <Picture
             src={joinPaths(
@@ -40,6 +44,7 @@ export default function CurrencyDisplay() {
                 ".svg",
             )}
             alt="test"
+            skeleton={selectedAsset.selectedAsset() == undefined}
           />
         </div>
       </div>
