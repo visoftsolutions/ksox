0314982d6c0df547c83d5d81a17b34d30645be73
Paweł <neotheprogramist@proton.me>
2023-08-24 20:43:26 +0200

    Contract Address Provider & updates

--- web/apps/processor/src/components/Deposit.tsx
+++ web/apps/processor/src/components/Deposit.tsx
@@ -1,17 +1,20 @@
 import Header from "~/components/Home/Header";
 import DepositDashboard from "~/components/Deposit/DepositDashboard";
+import { ContractAddressProvider } from "@packages/components/providers/ContractAddressProvider";
 
 export default function Deposit() {
   return (
-    <div class="grid grid-rows-[128px_1fr] h-full">
-      <div class="row-start-1 row-end-2">
-        <Header text="Deposit" />
-      </div>
-      <div class="row-start-2 row-end-3 relative overflow-clip">
-        <div class="absolute inset-0 overflow-y-auto">
-          <DepositDashboard />
+    <ContractAddressProvider>
+      <div class="grid grid-rows-[128px_1fr] h-full">
+        <div class="row-start-1 row-end-2">
+          <Header text="Deposit" />
+        </div>
+        <div class="row-start-2 row-end-3 relative overflow-clip">
+          <div class="absolute inset-0 overflow-y-auto">
+            <DepositDashboard />
+          </div>
         </div>
       </div>
-    </div>
+    </ContractAddressProvider>
   );
 }
--- web/apps/processor/src/components/Withdraw.tsx
+++ web/apps/processor/src/components/Withdraw.tsx
@@ -1,11 +1,14 @@
+import { ContractAddressProvider } from "@packages/components/providers/ContractAddressProvider";
 import Header from "~/components/Home/Header";
 
 export default function Withdraw() {
   return (
-    <div class="grid grid-rows-[128px_1fr] h-full">
-      <div class="row-start-1 row-end-2">
-        <Header />
+    <ContractAddressProvider>
+      <div class="grid grid-rows-[128px_1fr] h-full">
+        <div class="row-start-1 row-end-2">
+          <Header />
+        </div>
       </div>
-    </div>
+    </ContractAddressProvider>
   );
 }
