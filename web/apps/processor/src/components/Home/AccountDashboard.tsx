0a7c699d469c3f45872cd0402d0d07626ae6e006
Bartosz Nowak <bartekn337@gmail.com>
2023-08-25 13:46:57 +0200

    transfers when session

--- web/apps/processor/src/components/Home/AccountDashboard.tsx
+++ web/apps/processor/src/components/Home/AccountDashboard.tsx
@@ -19,46 +19,11 @@ import { z } from "zod";
 import { DisplayTransfer } from "@packages/types/transfer";
 import firstLastChars from "@packages/utils/firstLastChars";
 import { ev } from "@packages/types/primitives/fraction";
+import { Dynamic } from "solid-js/web";
+import { SessionResponse } from "@packages/components/providers/SessionProvider/models";
+import { session } from "@packages/components/providers/SessionProvider";
 
 export default function AccountDashboard() {
-  const assets = useAssets();
-  const assetsList = createMemo(() => [...assets().values()]);
-  const [transfers, setTransfers] = createSignal<ITransferElement[]>([]);
-
-  let eventsource: EventSource | undefined;
-
-  const convertTransfer = (element: DisplayTransfer): ITransferElement => {
-    return {
-      from:
-        element.from_user_name ||
-        firstLastChars(element.from_user_address, 4, 4),
-      to:
-        element.from_user_name ||
-        firstLastChars(element.from_user_address, 4, 4),
-      amount: ev(element.amount),
-      date: element.created_at,
-      symbol: element.asset_symbol,
-    };
-  };
-
-  onMount(async () => {
-    eventsource = await subscribeEvents(
-      `${api}/private/transfers/display`,
-      params({ limit: 10, offset: 0 }),
-      params({}),
-      (data) => {
-        setTransfers([
-          ...z.array(DisplayTransfer).parse(data).map(convertTransfer),
-          ...transfers(),
-        ]);
-      },
-    );
-  });
-
-  onCleanup(() => {
-    eventsource?.close();
-  });
-
   return (
     <div class="grid grid-rows-[auto_auto_auto_1fr] h-full gap-4">
       <CurrencyDisplay />
@@ -68,21 +33,67 @@ export default function AccountDashboard() {
       </p>
       <div class="relative">
         <div class="absolute inset-0 overflow-y-auto">
-          <div class="grid grid-flow-row gap-4 ">
-            <Index each={transfers()}>
-              {(element) => (
-                <TransferElement
-                  from={element().from}
-                  to={element().to}
-                  date={element().date}
-                  amount={element().amount}
-                  symbol={element().symbol}
-                />
-              )}
-            </Index>
-          </div>
+          <Dynamic component={CreateTransfers(session())} />
         </div>
       </div>
     </div>
   );
+}
+
+export function CreateTransfers(session: SessionResponse | undefined) {
+  return () => {
+    const [transfers, setTransfers] = createSignal<ITransferElement[]>([]);
+
+    let eventsource: EventSource | undefined;
+
+    const convertTransfer = (element: DisplayTransfer): ITransferElement => {
+      return {
+        from:
+          element.from_user_name ||
+          firstLastChars(element.from_user_address, 4, 4),
+        to:
+          element.to_user_address ||
+          firstLastChars(element.to_user_address, 4, 4),
+        amount: ev(element.amount),
+        date: element.created_at,
+        symbol: element.asset_symbol,
+      };
+    };
+
+    onMount(async () => {
+      if (session) {
+        eventsource = await subscribeEvents(
+          `${api}/private/transfers/display`,
+          params({ limit: 10, offset: 0 }),
+          params({}),
+          (data) => {
+            setTransfers([
+              ...z.array(DisplayTransfer).parse(data).map(convertTransfer),
+              ...transfers(),
+            ]);
+          },
+        );
+      }
+    });
+
+    onCleanup(() => {
+      eventsource?.close();
+    });
+
+    return (
+      <div class="grid grid-flow-row gap-4 ">
+        <Index each={transfers()}>
+          {(element) => (
+            <TransferElement
+              from={element().from}
+              to={element().to}
+              date={element().date}
+              amount={element().amount}
+              symbol={element().symbol}
+            />
+          )}
+        </Index>
+      </div>
+    );
+  };
 }
