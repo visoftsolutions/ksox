27c568d6ee772db816baed444292467820725d50
Bartosz Nowak <bartekn337@gmail.com>
2023-08-25 12:50:57 +0200

    display transfer zod

--- web/packages/types/transfer.ts
+++ web/packages/types/transfer.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 import { Datetime } from "./primitives/datetime";
 import { Fraction } from "./primitives/fraction";
 import { Uuid } from "./primitives/uuid";
+import { Address } from "@packages/components/providers/SessionProvider/models";
 
 export const Transfer = z.object({
   id: Uuid,
@@ -16,3 +17,21 @@ export const Transfer = z.object({
   fee: Fraction,
 });
 export type Transfer = z.infer<typeof Transfer>;
+
+export const DisplayTransfer = z.object({
+  id: Uuid,
+  created_at: Datetime,
+  from_user_id: Uuid,
+  from_user_address: Address,
+  from_user_name: z.string().optional(),
+  to_user_id: Uuid,
+  to_user_address: Address,
+  to_user_name: z.string().optional(),
+  asset_id: Uuid,
+  asset_icon_path: z.string(),
+  asset_name: z.string(),
+  asset_symbol: z.string(),
+  amount: Fraction,
+  fee: Fraction,
+});
+export type DisplayTransfer = z.infer<typeof DisplayTransfer>;
