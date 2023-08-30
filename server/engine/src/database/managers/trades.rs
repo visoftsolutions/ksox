475ac143e6b1b33c0146ea8d3d797870e6f44208
Paweł <neotheprogramist@proton.me>
2023-08-30 10:41:55 +0200

    fix insert maker_id

--- server/engine/src/database/managers/trades.rs
+++ server/engine/src/database/managers/trades.rs
@@ -14,13 +14,14 @@ impl TradesManager {
         sqlx::query!(
             r#"
             INSERT INTO spot.trades
-                (quote_asset_id, base_asset_id, taker_id, taker_presentation, order_id, price, taker_quote_volume, maker_quote_volume, taker_quote_volume_transfer_id, maker_quote_volume_transfer_id, last_modification_at, created_at)
+                (quote_asset_id, base_asset_id, taker_id, maker_id, taker_presentation, order_id, price, taker_quote_volume, maker_quote_volume, taker_quote_volume_transfer_id, maker_quote_volume_transfer_id, last_modification_at, created_at)
             VALUES
-                ($1, $2, $3, $4, $5, $6::fraction, $7::fraction, $8::fraction, $9, $10, $11, $12)
+                ($1, $2, $3, $4, $5, $6, $7::fraction, $8::fraction, $9::fraction, $10, $11, $12, $13)
             "#,
             element.quote_asset_id,
             element.base_asset_id,
             element.taker_id,
+            element.maker_id,
             element.taker_presentation,
             element.order_id,
             element.taker_price.to_tuple_string() as _,
